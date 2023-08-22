75bff66f4ebb8413f0dc0b05d106096e3392af8d
JUAN SILVA <54045471+dderking@users.noreply.github.com>
2023-08-22 17:09:05 -0300

    initiate frontend

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/require-render-return */
+import React, { Component } from 'react';
+
+class Button extends Component {
+	render() {
+		// ...
+	}
+}
+
+export default Button; // Don’t forget to use export default!
