62a3db82210a4bd4081fc9d30f9615b91aac3a71
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2023-09-17 14:11:41 -0300

    refactor pages

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,13 +4,13 @@ import { ChevronRightIcon } from '@chakra-ui/icons';
 interface ButtonProps {
     text: string
 }
-const Button : React.FC<ButtonProps> = ({ text }) => {
+const ButtonArrow : React.FC<ButtonProps> = ({ text }) => {
     return (
         <ChakraButton bgColor={'#F1AC81'} color='white'> 
-        <p> {text} </p> 
+        {text}
         <ChevronRightIcon pt={1} h={6} w={6} color='white' />
         </ChakraButton>
     )
 }
 
-export default Button;
+export default ButtonArrow;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import Button from "../components/button";
+import ButtonArrow from "../components/button";
 import Logo from "../components/logo";
 
 function Home() {
     return <>
         <Logo/>
-        <Button text="Buscar"/>
+        <ButtonArrow text="Buscar"/>
     </>;
 }
 
