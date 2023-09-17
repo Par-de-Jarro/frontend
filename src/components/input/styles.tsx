959887b6b3312e22f70bd648b15bb0fc97b8f70d
Natalia Salvino <nataliasalvino86@gmail.com>
2023-09-17 11:22:27 -0300

    adding form

--- src/components/input/styles.tsx
+++ src/components/input/styles.tsx
@@ -5,3 +5,12 @@ export const InputDiv = styled.div`
     flex-direction: column;
     width: 90%;
 `
+
+export const InputBox = styled.input`
+    height: 40px;
+    border-radius: 8px;
+    border: none;
+    padding-left: 10px;
+    font-size: 22px;
+    color: #606060;
+`
