46c114a7885d4b6690cc660481cc9f22a65f786a
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-28 10:39:11 -0300

    feat: search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -15,14 +15,11 @@ export default function SearchPage() {
       <Header/>
       <PageContainer>
         <CardsContainer>
-        {spots.spots.map((spot) => (
+        { 
+          spots.spots.map((spot) => (
           <Card title={spot.name} distance={spot.distance} empty_quota={spot.personal_quota} value={spot.value}/>
-          ))}
-          {/* <Card title='Lindo Apartamento no Benvenutto' distance={0.1} empty_quota={2} value={450}/>
-          <Card title='Prime Residence' distance={0.5} empty_quota={3} value={250}/>
-          <Card title='Prime Residence' distance={0.5} empty_quota={3} value={250}/>
-          <Card title='Prime Residence' distance={0.5} empty_quota={3} value={250}/>
-          <Card title='Prime Residence' distance={0.5} empty_quota={3} value={250}/> */}
+          ))
+        }
         </CardsContainer>
       </PageContainer>
       <NavBar/>
