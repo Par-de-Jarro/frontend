d4c17a4e4d8ec04b6bb855599793517c824e37b9
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-27 23:26:31 -0300

    feat: only show navbar on desktop

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Container, NavButton, UserIcon, SearchIcon, BillsIcon, HouseIcon } from './styles';
+import { isDesktop } from '../../utils/window';
 
-const Header: React.FC = () => (
-  <Container>
-    <NavButton isSelected={true}><UserIcon/></NavButton>
-    <NavButton><SearchIcon/></NavButton>
-    <NavButton><BillsIcon/></NavButton>
-    <NavButton><HouseIcon/></NavButton>
-  </Container>
+const NavBar: React.FC = () => (
+  <>
+  {
+  !isDesktop ? 
+  (
+    <Container>
+      <NavButton isSelected={true}><UserIcon/></NavButton>
+      <NavButton><SearchIcon/></NavButton>
+      <NavButton><BillsIcon/></NavButton>
+      <NavButton><HouseIcon/></NavButton>
+    </Container>
+  ) 
+    : null
+  }
+  </>
 );
 
-export default Header;
+export default NavBar;
---
+++ src/utils/window.ts
@@ -0,0 +1,7 @@
+let isDesktop = window.innerWidth > 768; // Define a largura de quebra entre desktop e mobile
+
+window.addEventListener('resize', () => {
+  isDesktop = window.innerWidth > 768;
+});
+
+export { isDesktop };
