93cb42078340d124d531c95ff5287358b245ef5d
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-27 21:57:00 -0300

    feat: header bar

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Container } from './styles';
+import { FaSearch } from 'react-icons/fa'
+import { TbAdjustmentsHorizontal } from 'react-icons/tb'
+import { Container, Input, SearchButton, FilterButton } from './styles';
 const SearchBar: React.FC = () => (
   <Container>
+    <FilterButton><TbAdjustmentsHorizontal color='#513422'/></FilterButton>
+    <Input placeholder='Qualquer Lugar'/>
+    <SearchButton><FaSearch color='#513422'/></SearchButton>
   </Container>
 );
 
--- src/components/search-bar/styles.ts
+++ src/components/search-bar/styles.ts
@@ -1,10 +1,52 @@
 import styled from "styled-components";
 
-export const Container = styled.input`
+export const Container = styled.div`
     display: flex;
-    flex: auto;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5px;
+    flex-direction: row;
     width: 100%;
-    height: 50px;
-    cursor: pointer;
+    height: 70px;
+    background: #ffffff;
+    border-radius: 50px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+    padding: 5px 15px 5px 15px;
     border: 0;
+`
+
+export const Input = styled.input`
+    width: 100%;
+    height: 100%;
+    border: 0;
+    background-color: transparent;
+`
+
+
+export const SearchButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 45px;
+    max-height: 45px;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    background-color: #f8f4e8;
+    border: 0;
+    padding: 0;
+`
+
+export const FilterButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 45px;
+    max-height: 45px;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    border: 1px solid #f8f4e8;
+    padding: 0;
 `
