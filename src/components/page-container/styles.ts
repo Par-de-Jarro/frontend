7eff211e558eb4baa75a261a737bf18a4856c429
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-10-01 13:46:59 -0300

    feat: primitive signUp page

--- src/components/page-container/styles.ts
+++ src/components/page-container/styles.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
   background-color: #ffff;
   padding: 35px;
   width: 100%;
--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageContainer from '../../components/page-container'
 import styled from 'styled-components'
 import Input from '../../components/input'
+import { NavLink, useNavigate } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5'
+import { Recommendation } from '../../types/input';
+import api from '../../services/api';
 
 const Title = styled.p`
     height: 100%;
@@ -16,36 +20,178 @@ const Title = styled.p`
     margin-bottom: 20px;
 `
 
+const Form = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  width: 100%;
+  height: 100%;
+`
+
+const CloseIcon = styled(IoClose)`
+  height: 25px;
+  width: 25px;
+  color: #513422;
+  cursor: pointer;
+`
+export const Button = styled.button`
+    border: 0;
+    height: 50px;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items:  center;
+
+    text-decoration: none;
+    font-size: 16px;
+
+    background-color: #f8f4e8;
+    color: #513422;
+    border-radius: 12px;
+
+
+    &:hover {
+        background-color: #513422;
+        color: #f8f4e8;
+    }
+    
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+`
+
 export default function SignIn () {
   const [name, setName] = useState('')
+  const [gender, setGender] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [bio, setBio] = useState('')
+  const [cellphone, setCellphone] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [course, setCourse] = useState('')
   const [university, setUniversity] = useState('')
+  const navigate = useNavigate();
 
-  const recommendations = [
-    {
-      "label": "Universidade Federal de Campina Grande",
-      "value": "UUID1"
-    },
-    {
-      "label": "Universidade Estadual da Paraíba",
-      "value": "UUID2"
-    }
-  ]
+  const [universityRecommendations, setUniversityRecommendations] = useState<Array<Recommendation>>([])
+  const [genderRecommendations, setGenderRecommendations] = useState<Array<Recommendation>>([])
+
+	const goBack = () => {
+		navigate(-1);
+	}
+
+  const getUniversities = () => {
+    (
+      api.get('/university').then((response) => {
+        const universities = response.data.map((elem : { name: string, id_university: string } ) => {
+            return {
+              value: elem.id_university,
+              label: elem.name
+            }
+        })
+
+        setUniversityRecommendations(universities)
+      })
+    )
+  }
+
+  const getGenders = () => {
+    const genders = [
+      {
+        value: "female",
+        label: "Feminino"
+      },
+      {
+        value: "male",
+        label: "Masculino"
+      },
+      {
+        value: "non-binary",
+        label: "Não binário"
+      },
+      {
+        value: "uninformed",
+        label: "Desejo não Informar"
+      }
+    ]
+
+    setGenderRecommendations(genders)
+  }
+  
+
+  useEffect(() => {
+    getGenders()
+    getUniversities()
+  }, [])
 
   return (
       <PageContainer>
-        <Title>Cadastre-se no Par de Jarro</Title>
+        <Form>
+          <TitleContainer>
+            <CloseIcon onClick={goBack} size={30} color='black'/>
+            <Title>Cadastre-se no Par de Jarro</Title>
+          </TitleContainer>
           <Input 
             label='Nome Completo' 
             inputValue={name}
             onInputValueChange={setName}
           />
           <Input 
-            recommendations={recommendations} 
+            recommendations={genderRecommendations} 
+            onSelectItem={(item) => {console.log(item.value);}} 
+            label='Gênero' 
+            inputValue={gender} 
+            onInputValueChange={setGender}
+          />
+          <Input 
+            label='Email' 
+            inputValue={email}
+            onInputValueChange={setEmail}
+          />
+          <Input 
+            label='Senha' 
+            inputValue={password}
+            onInputValueChange={setPassword}
+          />
+          <Input 
+            label='Bio' 
+            inputValue={bio}
+            onInputValueChange={setBio}
+          />
+          <Input 
+            label='Telefone' 
+            inputValue={cellphone}
+            onInputValueChange={setCellphone}
+          />
+          <Input 
+            label='CPF' 
+            inputValue={cpf}
+            onInputValueChange={setCpf}
+          />
+          <Input 
+            label='Curso' 
+            inputValue={course}
+            onInputValueChange={setCourse}
+          />
+          <Input 
+            recommendations={universityRecommendations} 
             onSelectItem={(item) => {console.log(item.value);}} 
             label='Universidade' 
             inputValue={university} 
             onInputValueChange={setUniversity}
           />
+          <Button onClick={() => {}}> Criar Conta </Button>
+        </Form>
       </PageContainer>
   )
 
