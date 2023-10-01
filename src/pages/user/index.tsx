ba39f2a29b2f7177d90c8360a2ab54cb767c6dfb
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-10-01 10:49:45 -0300

    feat: user nav bar

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,38 +1,56 @@
 import React from 'react'
-import { CardsContainer, Title, UserContainer, UserImage, UserInfoContainer, UserName } from './styles'
+import { CardsContainer, Title, LoggedInUserContainer, LoggedOffUserContainer, UserImage, UserInfoContainer, UserName, Button, RedirectLink } from './styles'
 import NavBar from '../../components/nav-bar'
 import PageContainer from '../../components/page-container'
 import NavCard from '../../components/nav-card'
 import { BsHouses } from 'react-icons/bs'
 import { PiUserCircleFill } from 'react-icons/pi'
 import { FaMoneyBillWave } from 'react-icons/fa'
+import { useAuth } from '../../hooks/auth'
 
-const UserPage: React.FC = () => (
-  <>
-    <PageContainer>
-      <Title>Perfil</Title>
-      <UserContainer to='/'>
-        <UserImage src='https://avatars.githubusercontent.com/u/38543529?v=4'/>
-        <UserInfoContainer>
-            <UserName>Antonio Neto</UserName>
-            Mostrar Perfil
-        </UserInfoContainer>
-      </UserContainer>
-      <CardsContainer>
-        <Title>Configurações</Title>
-        <NavCard label='Meu Perfil' redicted_to='/'>
-          <PiUserCircleFill />
-        </NavCard>
-        <NavCard label='Minhas Contas' redicted_to='/'>
-          <FaMoneyBillWave/>
-        </NavCard>
-        <NavCard label='Meus Locais' redicted_to='/'>
-          <BsHouses/>
-        </NavCard>
-      </CardsContainer>
-    </PageContainer>
-    <NavBar/>
-  </>
-)
+const UserPage: React.FC = () => {
+  const { user } = useAuth()
+
+  return (
+      <>
+        <PageContainer>
+          <Title>Perfil</Title>
+          { user && (
+              <LoggedInUserContainer to='/'>
+                <UserImage src='https://avatars.githubusercontent.com/u/38543529?v=4'/>
+                  <UserInfoContainer>
+                      <UserName>Antonio Neto</UserName>
+                      Mostrar Perfil
+                  </UserInfoContainer>
+              </LoggedInUserContainer>
+            )
+          }
+          { !user && (
+              <LoggedOffUserContainer>
+                <Button to=''>Entrar</Button>
+                Você ainda não tem conta? <RedirectLink to='/'>Cadastrar-se</RedirectLink>
+              </LoggedOffUserContainer>
+            )
+          }
+          <CardsContainer>
+            <Title>Configurações</Title>
+            <NavCard label='Meu Perfil' redicted_to='/'>
+              <PiUserCircleFill />
+            </NavCard>
+            <NavCard label='Minhas Contas' redicted_to='/'>
+              <FaMoneyBillWave/>
+            </NavCard>
+            <NavCard label='Meus Locais' redicted_to='/'>
+              <BsHouses/>
+            </NavCard>
+          </CardsContainer>
+        </PageContainer>
+        <NavBar/>
+      </>
+    )
+
+
+
+}
 
 export default UserPage
--- src/pages/user/styles.ts
+++ src/pages/user/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 import { NavLink } from 'react-router-dom'
 
-export const UserContainer = styled(NavLink)`
+export const LoggedInUserContainer = styled(NavLink)`
     width: 100%;
     display: flex;
     flex-direction: row;
@@ -11,10 +11,27 @@ export const UserContainer = styled(NavLink)`
     border-bottom: 1px solid #ebebeb;
     padding-bottom: 10px;
     text-decoration: none;
-    
+
     &:hover {
         background-color: #f2f2f2;
     }
+`
+
+export const LoggedOffUserContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    border-bottom: 1px solid #ebebeb;
+    padding-bottom: 10px;
+
+    color: #717171;
+`
+
+export const RedirectLink = styled(NavLink)`
+    color: #2b2b2b;
+
 `
 
 export const Title = styled.p`
@@ -57,5 +74,28 @@ export const UserImage = styled.img`
     width: 80px;
     height: 80px;
     border-radius: 50%;
+`
 
+export const Button = styled(NavLink)`
+    height: 50px;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items:  center;
+
+    text-decoration: none;
+    font-size: 16px;
+
+    background-color: #f8f4e8;
+    color: #513422;
+    border-radius: 12px;
+
+
+    &:hover {
+        background-color: #513422;
+        color: #f8f4e8;
+    }
+    
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
 `
