4b628d6629a633731f9dda1b6a9c3aa84c147468
Natalia Salvino <nataliasalvino86@gmail.com>
2023-10-05 00:53:13 -0300

    fix auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,20 @@
 import axios from 'axios'
 
-const token = localStorage.getItem('@ParDeJarro:token');
-
 const api = axios.create({
   baseURL: 'http://18.117.169.137',
   headers: {
     'Api-Key': 'zKVWMjBIxCxAfs40OqIdqgNwsyCIyCLMhL90T3t1iNOOt2G6M5wfgiej5mZPAIw',
-    'Authorization': `Bearer ${token}`, 
   }
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('@ParDeJarro:token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  }
+);
 
 export default api
