dadcf24a7cd3b86ee82e61c86463b2f72942a1de
Natalia Salvino <nataliasalvino86@gmail.com>
2023-10-07 21:46:57 -0300

    adding user page

---
+++ src/pages/userConfig/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { CardsContainer, Title, LoggedInUserContainer, LoggedOffUserContainer, UserImage, UserInfoContainer, UserName, Button, RedirectLink, LogOutButton } from './styles'
+import NavBar from '../../components/nav-bar'
+import PageContainer from '../../components/page-container'
+import NavCard from '../../components/nav-card'
+import { BsHouses } from 'react-icons/bs'
+import { PiUserCircleFill } from 'react-icons/pi'
+import { FaMoneyBillWave } from 'react-icons/fa'
+import { useAuth } from '../../hooks/auth'
+import { useNavigate } from 'react-router-dom'
+import UserPic from '../../styles/assets/User.jpg'
+
+const UserConfigPage: React.FC = () => {
+  const { user, signOut } = useAuth()
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    signOut()
+    navigate('/')
+  }
+
+  return (
+      <>
+        <PageContainer>
+          <Title>Perfil</Title>
+          { user && (
+              <LoggedInUserContainer to='/user/profile'>
+                <UserImage src={user.profile_img || UserPic}/>
+                  <UserInfoContainer>
+                      <UserName>{user.name}</UserName>
+                      Mostrar Perfil
+                  </UserInfoContainer>
+              </LoggedInUserContainer>
+            )
+          }
+          { !user && (
+              <LoggedOffUserContainer>
+                <Button to='/signIn'>Entrar</Button>
+                Você ainda não tem conta? <RedirectLink to='/signUp'>Cadastrar-se</RedirectLink>
+              </LoggedOffUserContainer>
+            )
+          }
+          <CardsContainer>
+            <Title>Configurações</Title>
+            <NavCard label='Meu Perfil' redicted_to='/user/profile'>
+              <PiUserCircleFill />
+            </NavCard>
+            <NavCard label='Minhas Contas' redicted_to='/'>
+              <FaMoneyBillWave/>
+            </NavCard>
+            <NavCard label='Meus Locais' redicted_to='/'>
+              <BsHouses/>
+            </NavCard>
+            {
+              user && (
+                <LogOutButton onClick={logOut}>
+                  Deslogar
+                </LogOutButton>
+              )
+            }
+          </CardsContainer>
+        </PageContainer>
+        <NavBar/>
+      </>
+    )
+
+
+
+}
+
+export default UserConfigPage
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SearchPage from './pages/search'
-import UserPage from './pages/user'
+import UserConfigPage from './pages/userConfig'
 import SignUp from './pages/signUp'
 import SignIn from './pages/signIn'
 import AddSpot from './pages/spot'
 import AddSpotImage from './pages/spot/image'
 import UserProfile from './pages/userProfile'
+import UserPage from './pages/user'
 import SpotDetails from './pages/spotDetails'
 
 export default function AppRoutes () {
@@ -13,6 +14,7 @@ export default function AppRoutes () {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SearchPage />} />
+        <Route path="/userConfig" element={<UserConfigPage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/signIn" element={<SignIn />} />
