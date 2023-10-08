287de9b958bf253641146399cedb4bd58740def3
Natalia Salvino <nataliasalvino86@gmail.com>
2023-10-08 01:54:04 -0300

    adding user page

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../../components/nav-bar'
 import PageContainer from '../../components/page-container'
-import { UserImage, Title, MainDiv, AboutSection } from './styles'
+import { UserImage, Title, MainDiv, AboutSection, SubInfo, UniIcon, CourseIcon, GenderIcon, AgeIcon } from './styles'
 import UserPic from '../../styles/assets/User.jpg'
 import { useNavigate } from 'react-router-dom'
 
@@ -11,15 +11,32 @@ const UserPage: React.FC = () => {
 
   return (
       <>
+        <MainDiv>
+          <UserImage src={UserPic}/>
+          <Title>User name</Title>
+        </MainDiv>
         <PageContainer>
-            <MainDiv>
-                <UserImage src={UserPic}/>
-                <Title>User name</Title>
-            </MainDiv>
             <AboutSection>
                 <p>Sobre</p>
                 <p>descrição descrição descrição</p>
             </AboutSection>
+            <SubInfo>
+              <GenderIcon></GenderIcon>
+              <p>Genero x</p>
+            </SubInfo>
+            <SubInfo>
+              <AgeIcon></AgeIcon>
+              <p>Idade x</p>
+            </SubInfo>
+            <SubInfo>
+              <UniIcon></UniIcon>
+              <p>Universidade x</p>
+            </SubInfo>
+            <SubInfo>
+              <CourseIcon></CourseIcon>
+              <p>Curso x</p>
+            </SubInfo>
+            <div>Contato</div>
         </PageContainer>
         <NavBar/>
       </>
