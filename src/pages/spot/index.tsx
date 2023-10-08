6586595b8cee6ec1ab14f6ba447cd1927623c41c
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2023-10-07 23:53:28 -0300

    fix numeric input

--- src/pages/spot/index.tsx
+++ src/pages/spot/index.tsx
@@ -137,7 +137,6 @@ export default function AddSpot() {
                     onInputValueChange={(value) => setAllowSmoker(value === "")}
                     type='checkbox'
                 />
-                                
                 <Button onClick={createSpot}>Continuar</Button>
             </Form>
 
