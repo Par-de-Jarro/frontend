c5836d7e4fc1f8ede6bd04cb5f27401347aafd7d
ekarani <ekarani.silvestre@ccc.ufcg.edu.br>
2023-10-11 00:55:09 +0000

    correct deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo mv /var/www/html/frontend /var/www/html/$(date +%s%N)
 sudo mkdir /var/www/html/frontend
-sudo chmod -R 775 frontend/
-sudo chown -R ubuntu:www-data frontend
+sudo chmod -R 775 /var/www/html/frontend
+sudo chown -R ubuntu:www-data /var/www/html/frontend
+#sudo rm -r /home/ubuntu/auxiliary
+#mkdir /home/ubuntu/auxiliary
 
 sudo systemctl restart apache2 
+sudo systemctl reload apache2
