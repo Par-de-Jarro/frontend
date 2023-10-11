cb271c0330788106bbfb650bab52d82c51628ed1
Ekarani <56440778+ekarani@users.noreply.github.com>
2023-10-10 23:03:31 -0300

    Update start-app.sh

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(cd /var/www/html/frontend && npm install --force && npm start &)
+(cd /var/www/html/frontend && npm install --force )
 sudo systemctl enable node-project
 sudo systemctl start node-project
 sudo systemctl restart apache2
